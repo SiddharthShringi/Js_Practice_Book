c71138a9fd7616fe33f9591f5697f6afbf9ac949
BlackSamsung <siddharthshringi@gmail.com>
2018-11-15 16:34:24 +0530

    make person constructor function

--- Day16_prototype/day16.js
+++ Day16_prototype/day16.js
@@ -1,49 +1,46 @@
 // The Car and the Truck have similar behavior and properties.
 // Rewrite them in a way such that they share those properties.
 
-var Vehicle = function(driver) {
-    this.driver = driver;
-    this.speed = 0;
-    this.drive = function(mph) {
-      this.speed = mph;
-      return this.driver + ' driving at ' + mph + ' miles per hour';
-    };
-  };
+// var Vehicle = function(driver, type=car) {
+//     if (type == 'car') {
+//         let this.
+//     }
+//   var Car = function(driver) {
+//     this.driver = driver;
+//     this.speed = 0;
+//     this.drive = function(mph) {
+//       this.speed = mph;
+//       return this.driver + ' driving at ' + mph + ' miles per hour';
+//     };
+//   };
 
-  Vehicle.prototype.loadCargo = function(cargo) {
-    this.container = [];
-    this.container.push(cargo)
-    return this.container
-  }
+//   var Truck = function(driver) {
+//     this.driver = driver;
+//     this.speed = 0;
+//     this.cargo = [];
+//     this.drive = function(mph) {
+//       this.speed = mph;
+//       return this.driver + ' driving at ' + mph + ' miles per hour';
+//     };
 
-  Vehicle.prototype.unloadCargo = function() {
-      return this.container.pop();
-  }
+//     this.loadCargo = function(cargo) {
+//       this.cargo.push(cargo);
+//       return this;
+//     };
 
-  var Car = function(driver) {
-    this.driver = driver;
-    this.speed = 0;
-    this.drive = function(mph) {
-      this.speed = mph;
-      return this.driver + ' driving at ' + mph + ' miles per hour';
-    };
-  };
+//     this.unloadCargo = function() {
+//       return this.cargo.pop();
+//     };
+//   };
 
-  var Truck = function(driver) {
-    this.driver = driver;
-    this.speed = 0;
-    this.cargo = [];
-    this.drive = function(mph) {
-      this.speed = mph;
-      return this.driver + ' driving at ' + mph + ' miles per hour';
-    };
 
-    this.loadCargo = function(cargo) {
-      this.cargo.push(cargo);
-      return this;
-    };
+// // Create an object called Teacher derived from the Person class, 
+// // and implement a method called teach which receives a string called subject, and prints out.
 
-    this.unloadCargo = function() {
-      return this.cargo.pop();
-    };
-  };
+function Person(name) {
+    this.name = name
+}
+
+Person.prototype.teach = function(subject) {
+    console.log(`${this.name} teaches ${subject}.`)
+}

5ade8ab0e3371d3b3d57b300db78818912938851
BlackSamsung <siddharthshringi@gmail.com>
2018-11-15 14:55:21 +0530

    make object with new keyword

--- objects/object.js
+++ objects/object.js
@@ -21,7 +21,7 @@ function createStudentData(p,c,m) {
         resultObj.maths = (student.marks.maths>=40) ? 'Pass' : 'Fail';
         resultObj.physics = (student.marks.physics>=40) ? 'Pass' : 'Fail';
         resultObj.chemistry = (student.marks.chemistry>=40) ? 'Pass' : 'Fail';
-        resultObj.finalReault = (resultObj.maths || resultObj.physics || resultObj.chemistry === 'Fail') ? 'Fail' : 'Pass';
+        resultObj.finalReault = (resultObj.maths === 'Pass' || resultObj.physics=== 'Pass' || resultObj.chemistry === 'Pass') ? 'Pass' : 'Fail';
         return resultObj;
     }
 
@@ -29,3 +29,48 @@ function createStudentData(p,c,m) {
 }
 
 
+//Another way of creating a object
+
+let studentFunction = {
+    total: function() {
+        let sum = 0;
+        for (let value of Object.values(this.marks)){
+            sum += value;
+        }
+        return sum;
+    },
+
+    percent: function() {
+        return this.total()/Object.values(this.marks).length;
+    },
+
+    result: function() {
+        resultObj = {}
+        resultObj.maths = (student.marks.maths>=40) ? 'Pass' : 'Fail';
+        resultObj.physics = (student.marks.physics>=40) ? 'Pass' : 'Fail';
+        resultObj.chemistry = (student.marks.chemistry>=40) ? 'Pass' : 'Fail';
+        resultObj.finalReault = (resultObj.maths === 'Pass' || resultObj.physics=== 'Pass' || resultObj.chemistry === 'Pass') ? 'Pass' : 'Fail';
+        return resultObj;
+    }
+}
+
+// function studentObject(p, c, m) {
+//     let newStudent = Object.create(studentFunction);
+//     newStudent.physics = p;
+//     newStudent.maths = m;
+//     newStudent.chemistry = c;
+//     return newStudent;
+// }
+
+
+
+// Object with prototype
+function User(p,c,m) {
+    this.physics = p;
+    this.maths = m;
+    this.chemistry = c;
+}
+
+User.prototype.total = function() {
+    return (this.physics + this.chemistry + this.maths);
+}

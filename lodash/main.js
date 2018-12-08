693157a9c2dc4369e1b223a0b8e9beb471e51fd1
BlackSamsung <siddharthshringi@gmail.com>
2018-12-08 12:52:03 +0530

    make compact and concat function

--- lodash/main.js
+++ lodash/main.js
@@ -11,3 +11,34 @@ function chunk(arr, num) {
     }
     return newArray;
 }
+// Example
+// chunk([1,2,3,4,5,6,7,8,9,],4);
+// output
+// [1,2,3,4],[5,6,7,8],[9]]
+
+
+// Creates an array with all falsey values removed. 
+// The values false, null, 0, "", undefined, and NaN are falsey.
+function compact(array) {
+    return acc = array.filter(v => Boolean(v))
+}
+// Example
+// compact([1,2,0,false, NaN]);
+// output [1, 2]
+// compact([1,2,0,false, NaN, 5]);
+// output [1, 2, 5]
+
+
+// Creates a new array concatenating array with any additional arrays and/or values.
+function concat(...x) {
+    return x.reduce((acc, value) => {
+        if (Array.isArray(value)) {
+            for(let elm of value) {
+                acc.push(elm)
+            }
+        } else {
+            acc.push(value)
+        }
+        return acc;
+    }, [])
+}
